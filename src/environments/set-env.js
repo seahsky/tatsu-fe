2e54af9db9827be3125d0778f427988769ad2d77
Seah Ky <seahsky97@gmail.com>
2022-10-04 16:03:13 +0800

    chore: enable environment variable setting

---
+++ src/environments/set-env.js
@@ -0,0 +1,39 @@
+const setEnv = () => {
+  const fs = require('fs');
+  const writeFile = fs.writeFile;
+  // Configure Angular `environment.ts` file path
+  const targetPath = './src/environments/environment.ts';
+  // Load node modules
+  const colors = require('colors');
+  const appVersion = require('../../package.json').version;
+  require('dotenv').config({
+    path: 'src/environments/.env',
+  });
+  // `environment.ts` file structure
+  const envConfigFile = `export const environment = {
+    backendApiUrl: '${process.env.BACKEND_API_URL}',
+    defaultCollectionSymbol: '${process.env.DEFAULT_COLLECTION_SYMBOL}',
+    appVersion: '${appVersion}',
+    production: true,
+  };
+  `;
+  console.log(
+    colors.magenta(
+      'The file `environment.ts` will be written with the following content: \n'
+    )
+  );
+  writeFile(targetPath, envConfigFile, (err) => {
+    if (err) {
+      console.error(err);
+      throw err;
+    } else {
+      console.log(
+        colors.magenta(
+          `Angular environment.ts file generated correctly at ${targetPath} \n`
+        )
+      );
+    }
+  });
+};
+
+setEnv();
